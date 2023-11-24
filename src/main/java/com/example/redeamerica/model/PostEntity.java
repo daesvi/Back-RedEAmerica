3aab7cd65ca7ba90a28dc4263c824d1c216a3c68
daesvi <villegasdaniel4321@gmail.com>
2023-11-23 22:40:07 -0500

    the model layer is created

---
+++ src/main/java/com/example/redeamerica/model/PostEntity.java
@@ -0,0 +1,34 @@
+package com.example.redeamerica.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "posts")
+public class PostEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UserEntity userId;
+
+    @NotBlank
+    private String content;
+
+    private String mediaUrl;
+
+    @NotBlank
+    private LocalDateTime timestamp;
+}
---
+++ src/main/java/com/example/redeamerica/model/UserEntity.java
@@ -0,0 +1,48 @@
+package com.example.redeamerica.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String lastName;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String phone;
+
+    @NotBlank
+    private String userType;
+
+    @ManyToMany(fetch = FetchType.EAGER, targetEntity = RoleEntity.class, cascade = CascadeType.PERSIST)
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<RoleEntity> roles;
+
+    @OneToMany(mappedBy = "userId", cascade = CascadeType.ALL)
+    private List<PostEntity> posts;
+
+}
