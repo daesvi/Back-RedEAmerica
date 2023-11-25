3ae5954ea143f721323bf1b26708e70c502af8c7
Maria Martinez <mmariayennifer.25@gmail.com>
2023-11-25 16:15:01 -0500

    actualizando

--- src/main/java/com/example/redeamerica/model/ERequestStatus.java
+++ src/main/java/com/example/redeamerica/model/ERequestStatus.java
@@ -1,18 +1,8 @@
 package com.example.redeamerica.model;
 
 public enum ERequestStatus {
-    PENDIENTE(1),
-    APROBADO(2),
-    RECHAZADO(3);
-
-    private final int value;
-
-    ERequestStatus(int value) {
-        this.value = value;
-    }
-
-    public int getValue() {
-        return value;
-    }
+    PENDIENTE,
+    APROBADO,
+    RECHAZADO;
 }
 
